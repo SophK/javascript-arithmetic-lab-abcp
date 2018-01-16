c44a6a9721e15453fe2c98b078cd57a8e7d3f24f
Sophie Kane <skane0207@gmail.com>
2018-01-16 19:58:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-var number = 10
-function add5() {
-  number +=  5 
-  }
-  
-function divideBy3() {
-  number /= 3
-}  
+function makeInt(n) {
+  parseInt(n)
 
-divideBy3()
-
-console.log(number)
+}
